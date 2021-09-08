e549c58cfb8788e8b7d268724eb412afbd3a8a1a
TridiumX <TridiumX@gmail.com>
2021-09-08 17:58:22 -0400

    Removed Test for Jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.5.0"
+    id("maven")
 }
 
 group = "org.nobledev"
@@ -14,19 +15,9 @@ repositories {
 dependencies {
     implementation("org.litote.kmongo:kmongo-coroutine:4.2.7")
     implementation("ch.qos.logback:logback-classic:1.2.1")
-    testImplementation(platform("org.junit:junit-bom:5.7.2"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-
 }
 
 
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
-}
-
-tasks.test {
-    useJUnitPlatform()
-    testLogging {
-        events("passed", "skipped", "failed")
-    }
 }
